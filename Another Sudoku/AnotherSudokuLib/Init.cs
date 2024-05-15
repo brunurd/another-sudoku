76560f57cb2c2640b3bdea084b3fa86b2a719e00
Bruno Araujo <araujo.contato@gmail.com>
2024-05-15 20:20:55 -0300

    feat: added HTTPRequest node to init scene

--- Another Sudoku/AnotherSudokuLib/Init.cs
+++ Another Sudoku/AnotherSudokuLib/Init.cs
@@ -1,13 +1,24 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 namespace AnotherSudokuLib
 {
     public partial class Init : Node2D
     {
+        private HttpRequest _httpRequest;
+
         public override void _Ready()
         {
-            GD.Print("Hello Another Sudoku!");
+            _httpRequest = GetNode<HttpRequest>("HTTPRequest");
+            _httpRequest.RequestCompleted += (long result, long responseCode, string[] headers, byte[] body) =>
+            {
+                var json = Json.ParseString(body.GetStringFromUtf8());
+                GD.Print(json);
+            };
         }
     }
 }
+
+// get list
+// show buttons list
